5ec9706a324bd1ff07fab0bd118be833e42f93ba
Tom Bradley <bradth01@gmail.com>
2016-09-07 20:02:01 -0400

    u2d

--- index.ios.js
+++ index.ios.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
-import NavBar from './src/components/navBar.js';
+import NavBar from './src/components/workoutNavBar/navBar.js';
 
 class MaxLifts extends Component {
 
